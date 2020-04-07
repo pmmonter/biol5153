01c4ac22a754ef0a343b26e61937cb770c06de7a
Paola Monterroso-Diaz <pmmonter@uark.edu>
2020-04-06 20:06:25 -0500

    parse command line arguments with argparse, calculate GC content

--- parseGFF.py
+++ parseGFF.py
@@ -1,20 +1,44 @@
 # /usr/bin/env python3
 
-# define input file 
-infile = "watermelon.gff"
-gene = list()
+import argparse
+import csv
+from Bio import SeqIO
 
-# open and parse watermelon.gff file
-with open(infile, 'r') as wm:
-    for line in wm:
-        fields = line.split()
+# create an argument parser object
+parser = argparse.ArgumentParser(description = "This script parses a GFF file and does stuff with it")
 
-        # extract gene name and store in list    
-        if not fields[10].startswith('simil'):
-            gene.append(fields[10])
+# add positional arguments
+parser.add_argument("gff", help="name of the GFF file")
+parser.add_argument("fasta", help="name of the FASTA file")
 
-# sort in alphabetical order and print list
-gene.sort()
-print(gene)
+# parse the arguments
+args = parser.parse_args()
 
+# read and parse the FASTA file
+genome = SeqIO.read(args.fasta, 'fasta')
 
+# read GFF file, line by line
+with open(args.gff, 'r') as gff_file:
+	
+	# create a csv reader object
+	reader = csv.reader(gff_file, delimiter="\t")
+
+	for line in reader:
+		# skip blank lines
+		if not line:
+			continue
+		else:
+			feature = line[2]
+			
+			# test whether this is a CDS feature
+			if feature == 'CDS':
+				start = line[3]
+                                end   = line[4]
+				
+				# extract corresponding substring/sequence
+				sequence_aux = genome.seq[int(start):int(end)]
+				seq_len = len(sequence_aux)
+				GC_count = sequence_aux.count('C')+sequence_aux.count('G')
+				GC_percent = (GC_count/seq_len)*100
+				
+				print("GC content:","%.2f"%GC_percent+"%")
